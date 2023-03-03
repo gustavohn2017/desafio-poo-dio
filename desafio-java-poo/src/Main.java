6f2a42a5e27a1376f50356f6401c863038ecd4a0
GustavoHN2017 <gustavohn2017@gmail.com>
2023-03-03 14:40:23 -0300

    new commit

--- desafio-java-poo/src/Main.java
+++ desafio-java-poo/src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentorias;
 
 public class Main {
@@ -21,6 +23,41 @@ public class Main {
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria1);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Java Developer Bootcamp");
+        bootcamp.setDescricao("Lorem Ipsum dolor sit amet..");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Dev1");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos"+ dev1.getConteudosInscritos());
+        dev1.progredir();
+        dev1.progredir();
+        System.out.println("Conteudos inscritos"+ dev1.getConteudosInscritos());
+        System.out.println("Conteudos concluidos"+ dev1.getConteudosConcluidos());
+        System.out.println("XP:" + dev1.calcularTotalXP());
+
+        System.out.println("-------------");
+
+
+        
+        Dev dev2 = new Dev();
+        dev2.setNome("Dev2");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos"+ dev2.getConteudosInscritos());
+        dev2.progredir();
+        System.out.println("Conteudos concluidos"+ dev2.getConteudosConcluidos());
+        System.out.println("XP:" + dev2.calcularTotalXP());
+
+
+
+
+
+
 
     }
 }
